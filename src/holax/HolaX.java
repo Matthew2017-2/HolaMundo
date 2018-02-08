657f3e972a35f02688372f2c09ddf9a025c37ca6
Matthew2017-2 <izquierdo.mateo11@gmail.com>
2018-02-08 07:23:26 -0500

    XD

--- src/holax/HolaX.java
+++ src/holax/HolaX.java
@@ -1,21 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package holax;
 
-/**
- *
- * @author Estudiantes
- */
-public class HolaX {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class HolaX {
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        String x = sc.next();
+        System.out.println("hola mundo");
+        System.out.println("hola "+x);
     }
     
 }
